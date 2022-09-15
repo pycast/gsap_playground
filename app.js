f0fdb31cd91437249c10a0646cac7a555dd6d004
pycast <py.castelleta@gmail.com>
2022-09-15 14:45:20 +0200

    algo

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ console.log(imgs);
 
 const TL = gsap.timeline({paused:true});
 
+// gsap.set peut modifier les propriétés des cibles
+
+gsap.set(imgs, {scale: 0})
+
+// on utilise gsap.timeline.to pour modifier les propriétés 
+
+// ease : css transition 
+
 TL
 .to(imgs, {scale: 1, duration: 0.1, stagger: 0.25, ease: "elastic.out(1, 0.3)"});
 
